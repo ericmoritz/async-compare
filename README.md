50002a4899ce53aa56d471ff7945a53dd0d3f66d
Eric Moritz <eric@themoritzfamily.com>
2013-08-29 00:21:24 -0400

    Added an erlang example

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
-A comparison of different asynchronous platforms
+A comparison of different asynchronous platforms by simulating some data composition tasks
 
-## Firsts!
+## First! task
 
-The first task at hand is to do the following.
+The first! task is the following:
 
-1. Download a list of subreddits
+1. Download the data for a list of subreddits
 2. Download each post on the subreddit and extract the first comment
 3. Sort each comment by created
-
+4. print out the elapsed time
---
+++ erlang-plists/firsts.erl
@@ -0,0 +1,130 @@
+-module(firsts).
+
+-compile(export_all).
+
+%%--------------------------------------------------------------------
+%% Task
+%%--------------------------------------------------------------------
+main(Subreddits) ->
+    {TS, _} = timer:tc(fun() -> download_all(Subreddits) end),
+    io:format("~ws~n", [TS/1000000]).
+
+download_all(Subreddits) ->
+    Permalinks = lists:append(
+		   lists:append(
+		     plists:map(
+		       fun(S) -> parse_links(download_subreddit(S)) end,
+		       Subreddits))),
+    lists:sort(
+      fun(X,Y) -> lookup(<<"created">>, X) >= lookup(<<"created">>, Y) end, 
+      lists:append(
+	plists:map(
+	  fun(L) -> parse_first(download_post(L)) end,
+	  Permalinks))).
+    
+
+
+
+    
+
+
+%%--------------------------------------------------------------------
+%% Downloaders
+%%--------------------------------------------------------------------
+
+download_subreddit(Subreddit) ->
+    download_body(subreddit_url(Subreddit)).
+
+download_post(Permalink) ->
+    download_body(post_url(Permalink)).
+
+
+download_body(Url) ->
+    {ok, {_, _, Body}} = httpc:request(Url),
+    Body.
+
+
+%%--------------------------------------------------------------------
+%% URLs
+%%--------------------------------------------------------------------
+
+reddit_url() ->
+    "http://www.reddit.com".
+
+subreddit_url(Subreddit) ->
+    reddit_url() ++ "/r/" ++ Subreddit ++ ".json".
+
+post_url(Permalink) ->
+    reddit_url() ++ Permalink ++ ".json".
+
+%%--------------------------------------------------------------------
+%% Parsers
+%%--------------------------------------------------------------------
+
+parse_links(JSON) ->
+    permalinks(jiffy:decode(JSON)).
+
+parse_first(JSON) ->
+    first_comment(comments(jiffy:decode(JSON))).
+
+
+%%--------------------------------------------------------------------
+%% Getters
+%%--------------------------------------------------------------------
+
+-spec parse_links(any()) -> {ok, [string()]} | {error, any()}.
+permalinks(Entity) ->
+    lists:map(
+      fun permalink/1,
+      children(Entity)
+    ).
+
+
+permalink(Entity) ->
+    case lookup(<<"permalink">>, lookup(<<"data">>, Entity)) of
+	none ->
+	    [];
+	Link ->
+	    [binary_to_list(Link)]
+    end.
+
+children(Entity) ->
+    lookup(<<"children">>, 
+	   lookup(<<"data">>, Entity)).
+
+comments([_, CommentListing|_]) ->
+    children(CommentListing);
+comments(_) ->
+    [].
+
+first_comment([Comment|_]) ->
+    comment(Comment);
+first_comment([]) ->
+    [].
+
+comment(JSON) ->
+    Data = lookup(<<"data">>, JSON),
+    Body = lookup(<<"body">>, Data),
+    Created = lookup(<<"created">>, Data),
+    case {Body, Created} of
+	{none, none} ->
+	    [];
+	_ ->
+	    [
+	     {[{<<"body">>, Body},
+	       {<<"created">>, Created}]}
+	    ]
+    end.
+
+lookup(_, none) ->
+    none;
+lookup(Key, {Props}) ->
+    case proplists:lookup(Key, Props) of 
+	none -> none;
+	{_, V} -> V
+    end;
+lookup(_, _) ->
+    none.
+
+
+
