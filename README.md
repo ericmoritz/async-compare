4a7f315c1d32b7ebfcd895ff2a6f9879ea8f76ca
Eric Moritz <eric@themoritzfamily.com>
2013-08-29 15:07:29 -0400

    added a terrible node example

--- README.md
+++ README.md
@@ -1,10 +1,37 @@
-A comparison of different asynchronous platforms by simulating some data composition tasks
+This is a comparison of implementations of the same task in the various platform.
 
-## First! task
+The goal is to demostrate the ability to express the task at hand. 
 
-The first! task is the following:
+## Task: Firsts!
 
-1. Download the data for a list of subreddits
-2. Download each post on the subreddit and extract the first comment
-3. Sort each comment by created
-4. print out the elapsed time
+  1. Download N subreddits concurrently
+  2. Download each post concurrently
+  3. Collect the first comment of each post into a list
+  4. Sort the resulting list by the comment's creation date desc
+
+### URLs:
+
+  * subreddit: http://www.reddit.com/r/{subreddit}.json
+  * post: http://www.reddit.com//r/funny/comments/{id}.json
+
+### Reddit's Data Structure
+
+Reddit uses a homogeneous tree structure for all their pages.
+
+```json
+Listing	:: [Entity]
+Entity	:: {"kind": Kind, "data": {"id": String, "children": Maybe [Listing]}
+Kind	:: "Listing" | Post | Comment
+Post	:: "t3"
+Comment :: "t1"
+
+/* This is my bastardized Haskell inspired JSON type notation
+Capitalized tokens are Types.
+
+The Maybe prefix means that the the key may be present or maybe null if
+present.
+
+| means that the type could be any of the values.
+*/
+```
+
---
+++ node/firsts.js
@@ -0,0 +1,169 @@
+/**
+   Downloaders
+**/
+var http = require("http");
+var async = require("async");
+
+function downloadAll(subreddits, finalCallback) {
+    async.concat(subreddits, 
+		 function(subreddit, callback) {
+		     downloadSubReddit(subreddit, function(permalinks) {
+			 permalinks.bind(function(links) {
+			     callback(null, links);
+			 });
+		     });
+		 },
+
+		 function(err, permalinks) {
+		     async.concat(permalinks,
+				  function(permalink, callback) {
+				      downloadFirst(
+					  permalink, 
+					  function(first) {
+					      first.bind(function(comment) {
+						  callback(null, [comment]);
+					      });
+					  })
+				  },
+				  function(err, firsts) {
+				      finalCallback(firsts);
+				  })
+		 });
+}
+exports.downloadAll = downloadAll;
+
+function downloadBody(url, callback) {
+    http.get(url, function(res) {
+	var body = ""
+
+	res.on("data", function(chunk) {
+	    body += chunk;
+	});
+
+	res.on("end", function() {
+	    callback(body);
+	});
+
+    });
+}
+exports.downloadBody = downloadBody;
+
+function downloadSubReddit(subreddit, callback) {
+    var url = "http://www.reddit.com/r/" + subreddit + ".json";
+    downloadBody(url, function(body) {
+	callback(parseSubReddit(body));
+    });
+}
+exports.downloadSubReddit =  downloadSubReddit
+
+function downloadFirst(permalink, callback) {
+    var url = "http://www.reddit.com" + permalink + ".json";
+    downloadBody(url, function(body) {
+	var first = parseFirst(body);
+	callback(first);
+    });
+}
+exports.downloadFirst = downloadFirst;
+
+/** 
+    Parsers
+**/
+function parseSubReddit(body) {
+    var permalinksM = lift(maybe, permalinks);
+
+    return permalinksM(
+	children(JSON.parse(body))
+    );
+}
+exports.parseSubReddit = parseSubReddit;
+
+function parseFirst(body) {
+    var listing = JSON.parse(body);
+    var firstM = lift(maybe, first);
+    return firstM(comments(listing)).bind(makeComment);
+}
+exports.parseFirst = parseFirst;
+
+/**
+   Accessors
+**/
+function makeComment(entity) {
+    return maybe(entity.data).bind(
+	function(data) {
+	    var body = data.body;
+	    var created = data.created;
+	    return body && created 
+		? maybe({body: body, created: created}) 
+	        : maybe(null);
+	})
+}
+
+function first(list) {
+    return list[0];
+}
+
+function comments(listing) {
+    return maybe(listing[1]).bind(children)
+}
+
+function children(entity) {
+    return getNested(entity, "data", "children");
+}
+
+function permalinks(children) {
+    return children.reduce(
+	function(arr, entity) {
+	    permalink(entity).bind(function(x) {
+		arr.push(x);
+	    });
+	    return arr;
+	},
+	[]);
+}
+
+function permalink(entity) {
+    return getNested(entity, "data", "permalink");
+}
+
+/**
+   Utilities
+**/
+function lift(monad, f) {
+    return function(m) {
+	return m.bind(function(x) { return monad(f(x))});
+    }
+}
+exports.lift = lift;
+
+function maybe(x) {
+   return {
+       bind: function(f) { 
+	   if(x == null) {
+	       return maybe(null);
+	   } else {
+	       return f(x);
+	   }
+       },
+       getDefault: function(y) {
+	   return x == null ? y : x
+       }
+   }
+}
+exports.maybe = maybe;
+
+function lookup(key, obj) {
+    return function(obj) {
+	return maybe(obj[key]);
+    };
+};
+
+function getNested(obj) {
+    var keys = [].slice.call(arguments, 1);
+    return [].reduce.call(keys,
+	function(m, key) {
+	    return m.bind(lookup(key));
+	},
+	maybe(obj)
+    );
+}
+
